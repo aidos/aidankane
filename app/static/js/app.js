6fb4c3ac09e4b536a013bed3bf0aff1a5107b752
Aidan Kane <aidankane@gmail.com>
2012-11-12 21:54:36 +0000

    inital commit

---
+++ app/static/js/app.js
@@ -0,0 +1,15 @@
+
+// Define libraries
+require.config({
+	paths: {
+		jquery: 'libs/jquery-1.7.2.min'
+	},
+  shim: {
+  }
+});
+
+// Load our app
+define(['jquery', 'libs/ios-zoom-fix'], function() {
+
+});
+
